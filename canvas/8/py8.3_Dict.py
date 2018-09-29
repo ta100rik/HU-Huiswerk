597883189e10fb5e7f423e72196689b20da03a27
Zyad Osseyran <zyadosseyran@gmail.com>
2018-09-29 21:02:33 +0200

    Housework 8.3

---
+++ canvas/8/py8.3_Dict.py
@@ -0,0 +1,15 @@
+Programminggrades =	{
+  "Arthur": 6,
+  "Piet": 1,
+  "Jan": 10,
+  "Zyad": 10,
+  "Gert": 7,
+  "Arjan": 4,
+  "Willem": 2,
+  "Rick": 3,
+  "Bas": 3,
+  "Wesley": 3
+}
+for naam,cijfer in Programminggrades.items():
+    if cijfer > 9:
+        print(naam)
